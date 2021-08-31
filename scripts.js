75f413528fea791ece9f67471e8ace6ace468b52
TravinDreek <1244050218@qq.com>
2021-08-31 23:27:11 +0800

    Reconstruction

--- scripts.js
+++ scripts.js
@@ -1,40 +1,29 @@
-var active = false;
-var clicks;
-var button = document.getElementById("click");
-var info = document.getElementById("info");
+var clicks = 0;
 var testTime = 10;
+var button = document.getElementById("btn");
+var progressbar = document.getElementById("btnprog");
 
-function Click() {
-	if (active == false){
-		active = true;
-		button.innerText = "START"
-		clicks = 0;
-		button.className = "button button-primary button-glow button-rounded button-giant";
-		info.innerHTML = 'Time: <u onclick="Custom()">' + testTime + 's</u>'
+button.onclick = function() {
+	if(clicks == 0) {
+		setTimeout(timeout, testTime * 1000);
+		progressbar.style.width = '100%';
 	}
-	else{
-		if(clicks == 0){
-			clearTimeout(t);
-			var t = setTimeout("Timeout()", testTime * 1000);
-			button.innerText = "CLICK"
-		}
-		clicks ++;
-		info.innerText = "Clicks: " + clicks
-	}
+	clicks ++;
+	btn.innerText = clicks + ' clicks';
 }
-function Custom() {
-	var input = prompt("Input a integer:","10");
-	if (!(/(^[1-9]\d*$)/.test(input))) { 
-		alert("Invaild integer!");
-　　}else { 
-		testTime = input;
-		info.innerHTML = 'Time: <u onclick="Custom()">' + testTime + 's</u>'
-　　}
+
+function timeout() {
+	button.disabled = true;
+	button.innerText = clicks / testTime + ' CPS';
+	button.style.cursor = 'wait';
+	setTimeout(resetbutton, 3000);
+	progressbar.style.transition = 'width 3s';
+	progressbar.style.width = '0';
 }
-function Timeout() {
-	button.className = "button button-glow button-rounded button-highlight";
-	button.innerText = "RESTART";
-	info.innerText = "Avg: " + clicks / testTime + " CPS"
-	active = false;
-	alert("Average: " + clicks / testTime + " CPS");
+
+function resetbutton() {
+	button.disabled = false;
+	button.style.cursor = 'crosshair';
+	progressbar.style.transition = 'width 10s linear';
+	clicks = 0;
 }
